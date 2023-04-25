5994d15a73d5b545b9d1ba2ec6dffd3500a28997
Fatima Ali <fofoalmali@gmail.com>
2023-04-25 12:59:35 +0300

    Triangle Done

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ class Polygon{
         return this.sides.reduce((acc,current) => acc+= current , 0);
     }
 }
-class Triangle extends polygon{
+const polygon = new Polygon( [ 5, 5, 5 ] );
+
+class Triangle extends Polygon{
+    get isValid(){
+     const [a,b,c] =this.sides;
+     if( a+b>c && a+c>b && b+c>a){
+        return true;}
+     return false;
+    }
 
 }
-
-const polygon = new Polygon( [ 5, 5, 5 ] );
-const triangle = new Triangle( [ 5, 5, 5 ] )
+const triangle = new Triangle( [ 5, 5, 5 ]);
+console.log(triangle.countSides);
+console.log(triangle.perimeter);
+console.log(triangle.isValid)
+   
