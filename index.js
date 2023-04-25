777b20ef4eb0b6ff20b3e8886b2972fe32cbe5aa
Fatima Ali <fofoalmali@gmail.com>
2023-04-25 12:34:27 +0300

    Polyong Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+class Polygon{
+    constructor(sides){
+        this.sides=sides;
+    }
+    get countSides(){
+       return this.sides.length;
+    }
+    get perimeter(){
+        return this.sides.reduce((acc,current) => acc+= current , 0);
+    }
+}
+class Triangle extends polygon{
+
+}
+
+const polygon = new Polygon( [ 5, 5, 5 ] );
+const triangle = new Triangle( [ 5, 5, 5 ] )
